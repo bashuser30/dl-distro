7c8034eb41e4511bda1bbc0f3b7adc365f2dab01
bashuser30 <bashuser30@mailbox.org>
2024-01-05 00:01:38 -0500

    remove json file from installer

--- installer.sh
+++ installer.sh
@@ -11,14 +11,12 @@ prefix="/usr/local"
 license_dir="$prefix/share/licenses/dl-distro"
 man_dir="$prefix/share/man/man1"
 bin_dir="$prefix/bin"
-data_dir="$prefix/share/dl-distro"
 
 install_files()
 {
 	install -Dm644 LICENSE "$license_dir/LICENSE"
 	install -Dm644 man/dl-distro.1 "$man_dir/dl-distro.1"
 	install -Dm755 dl-distro "$bin_dir/dl-distro"
-	install -Dm644 data.json "$data_dir/data.json"
 }
 
 uninstall_files()
@@ -26,7 +24,6 @@ uninstall_files()
 	rm -f "$license_dir/LICENSE"
 	rm -f "$man_dir/dl-distro.1"
 	rm -f "$bin_dir/dl-distro"
-	rm -f "$data_dir/data.json"
 }
 
 case "$1" in
