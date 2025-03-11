431c3f95346cd0229677e48efaf73f9fb6f250f7
bashuser30 <bashuser30@mailbox.org>
2025-03-11 16:39:07 -0400

    Add scripts/update-aur.sh

---
+++ scripts/update-aur.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+REPO_URL="https://codeberg.org/bashuser30/dl-distro"
+PKGBUILD_DIR="$HOME/git/pkgbuilds/dl-distro"
+VERSION="$(<./VERSION)"
+VERSION="${VERSION#v}"
+
+cd "$PKGBUILD_DIR" || exit 1
+
+trap 'rm -f *.tar.gz *.tar.lz4' EXIT INT TERM
+
+sed -i "s/^pkgver=.*/pkgver=$VERSION/" PKGBUILD
+sed -i "s/^pkgrel=.*/pkgrel=1/" PKGBUILD
+
+if ! updpkgsums; then
+	printf "ERROR: updpkgsums failed\n" >&2
+	exit 1
+fi
+
+rm -f ./*.tar.gz
+
+if ! makepkg --clean; then
+	printf "ERROR: makepkg --clean failed\n" >&2
+	exit 1
+fi
+
+printf "\n"
+
+git diff PKGBUILD
+
+printf "\nCommit and push? (y/N): \n"
+
+read -r CHOICE
+
+if [[ "$CHOICE" != "y" && "$CHOICE" != "Y" ]]; then
+	printf "Canceled\n" >&2
+	exit 1
+fi
+
+makepkg --printsrcinfo >.SRCINFO
+
+git add PKGBUILD .SRCINFO
+git commit -m "v$VERSION" -m "$REPO_URL/releases/tag/v$VERSION"
+git push
