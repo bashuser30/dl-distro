c8dcbaa47729a78a4df8d154dd45613023a82a1e
bashuser30 <bashuser30@mailbox.org>
2025-08-02 17:20:12 -0400

    Add verbose flags to install and rm in installer script

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -14,18 +14,18 @@ BASH_COMPLETIONS_DIR="/usr/local/share/bash-completion/completions"
 
 install_files()
 {
-	install -Dm644 LICENSE "$LICENSE_DIR/LICENSE"
-	install -Dm644 man/dl-distro.1 "$MAN_DIR/dl-distro.1"
-	install -Dm755 dl-distro "$BIN_DIR/dl-distro"
-	install -Dm644 completions/bash "$BASH_COMPLETIONS_DIR/dl-distro"
+	install -Dvm644 LICENSE "$LICENSE_DIR/LICENSE"
+	install -Dvm644 man/dl-distro.1 "$MAN_DIR/dl-distro.1"
+	install -Dvm755 dl-distro "$BIN_DIR/dl-distro"
+	install -Dvm644 completions/bash "$BASH_COMPLETIONS_DIR/dl-distro"
 }
 
 uninstall_files()
 {
-	rm -f "$LICENSE_DIR/LICENSE"
-	rm -f "$MAN_DIR/dl-distro.1"
-	rm -f "$BIN_DIR/dl-distro"
-	rm -f "$BASH_COMPLETIONS_DIR/dl-distro"
+	rm -vf "$LICENSE_DIR/LICENSE"
+	rm -vf "$MAN_DIR/dl-distro.1"
+	rm -vf "$BIN_DIR/dl-distro"
+	rm -vf "$BASH_COMPLETIONS_DIR/dl-distro"
 }
 
 case "${1:-}" in
